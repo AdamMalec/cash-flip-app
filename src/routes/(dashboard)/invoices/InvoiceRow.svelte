9516871c3f8bc90a38b4b3ba4d66befd83fa703d
adam malec <add.malec@gmail.com>
2023-07-26 12:35:48 +0300

    refactor: add all icons to Icon COMPONENT

--- src/routes/(dashboard)/invoices/InvoiceRow.svelte
+++ src/routes/(dashboard)/invoices/InvoiceRow.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import AdditionalMenu from '$lib/components/AdditionalMenu.svelte';
 	import Tag from '$lib/components/Tag.svelte';
+	import IconThreeDots from '$lib/components/icons/IconThreeDots.svelte';
+	import IconView from '$lib/components/icons/IconView.svelte';
 	import { convertDate, isLate } from '$lib/utils/dateHelpers';
 	import { centsToDollars, sumLineItems } from '$lib/utils/moneyHelpers';
 
@@ -22,7 +24,7 @@
 		}
 	}
 
-		// убирать рамку у иконки по клику (blur())
+	// убирать рамку у иконки по клику (blur())
 </script>
 
 <li class="invoices__item invoice">
@@ -33,39 +35,17 @@
 		<li class="invoice__name">{invoice.client.name}</li>
 		<li class="invoice__amount">${centsToDollars(sumLineItems(invoice.lineItems))}</li>
 		<li class="invoice__view">
-			<a href="/"
-				><svg
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M24 11.5C24 11.5 18.9285 4 12 4C5.0715 4 0 11.5 0 11.5C0 11.5 5.0715 19 12 19C18.9285 19 24 11.5 24 11.5ZM12 16C9.5145 16 7.5 13.987 7.5 11.5C7.5 9.013 9.5145 7 12 7C14.484 7 16.5 9.013 16.5 11.5C16.5 13.987 14.484 16 12 16Z"
-					/>
-					<path
-						d="M12 13C12.8284 13 13.5 12.3284 13.5 11.5C13.5 10.6716 12.8284 10 12 10C11.1716 10 10.5 10.6716 10.5 11.5C10.5 12.3284 11.1716 13 12 13Z"
-					/>
-				</svg></a
-			>
+			<a href="/">
+				<IconView />
+			</a>
 		</li>
 		<li class="invoice__more">
-			<button on:click={() => (isAddMenuOpen = !isAddMenuOpen)} on:click={() => this.blur()}>
-				<svg
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<circle cx="5" cy="12" r="2" />
-					<circle cx="12" cy="12" r="2" />
-					<circle cx="19" cy="12" r="2" />
-				</svg>
+			<button on:click={() => (isAddMenuOpen = !isAddMenuOpen)}>
+				<IconThreeDots />
 			</button>
+
 			{#if isAddMenuOpen}
-				<AdditionalMenu {isAddMenuFull}/>
+				<AdditionalMenu {isAddMenuFull} />
 			{/if}
 		</li>
 	</ul>
