c164158539922afc8ded61399040611a9714e9c6
adam malec <add.malec@gmail.com>
2023-07-26 09:07:47 +0300

    feat: add Portal COMPONENT

---
+++ src/lib/components/Portal.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	function portal(node: HTMLElement) {
+		let target: HTMLElement | null;
+
+		function update() {
+			target = document.querySelector('body');
+			target?.appendChild(node);
+			node.hidden = false;
+		}
+
+		function destroy() {
+			if (node.parentNode) {
+				node.parentNode.removeChild(node);
+			}
+		}
+
+		update();
+
+		return { update, destroy };
+	}
+</script>
+
+<div class="portal" hidden use:portal>
+	<slot />
+</div>
