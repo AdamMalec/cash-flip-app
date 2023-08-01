e863689ba7369cc7f9b46b8aed6bc672792be543
adam malec <add.malec@gmail.com>
2023-08-01 18:38:39 +0300

    feat: add Overlay COMPONENT

---
+++ src/lib/components/Overlay.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+	<style>
+		html {
+			overflow: hidden;
+		}
+	</style>
+</svelte:head>
+
+<div class="overlay" />
+
+<style>
+	.overlay {
+		position: fixed;
+		inset: 0;
+
+		background-color: var(--pico-primary);
+		opacity: 0.6;
+
+		z-index: var(--z-overlay);
+	}
+</style>
