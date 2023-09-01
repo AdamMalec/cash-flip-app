1a5822a621154d03d3bae5f44d5e9e4ba1de8785
Adam Malec <add.malec@gmail.com>
2023-09-01 20:13:37 +0300

    feat: update stores

--- src/lib/stores/ClientStore.ts
+++ src/lib/stores/ClientStore.ts
@@ -4,5 +4,10 @@ import data from "../../seed.json";
 export const clients = writable<Client[]>([]);
 
 export function loadClients() {
-  clients.set(data.clients)
+  clients.set(data.clients);
+}
+
+export function addClient(clientToAdd: Client) {
+	clients.update((prev: Client[]) => [...prev, clientToAdd]);
+	return clientToAdd;
 }
