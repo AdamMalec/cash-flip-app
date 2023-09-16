3e0be9958eb3cf4ce4fc823e26c81c360d9385ee
Adam Malec <add.malec@gmail.com>
2023-09-16 22:57:11 +0300

    refactor: add auth pages styles to global.css

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <h1>Login</h1>
-<form>
+<form class="auth">
 	<div class="field">
 		<label for="email">Email</label>
 		<input type="text" name="email" id="email" required bind:value={email} />
@@ -19,10 +19,8 @@
 	</div>
 
 	<div class="field">
-		<button type="submit">Login</button>
-		<p>
-			<a href="/signup">Don't have an account yet?</a>
-		</p>
+		<button class="auth__btn" type="submit">Login</button>
+		<p><a href="/signup">Don't have an account yet?</a></p>
 	</div>
 </form>
 
@@ -32,48 +30,8 @@
 		color: var(--color-accent);
 	}
 
-	.field {
-		min-width: 19rem;
-	}
-	.field label {
-		color: var(--color-accent);
-	}
-
 	.field__wrapper {
 		display: flex;
 		justify-content: space-between;
-	}
-
-	.field a {
-    align-self: baseline;
-    display: inline-block;
-    padding: 0.1rem;
-    padding-bottom: 0;
-		font-size: 0.75rem;
-		text-decoration: underline;
-		color: var(--pico-primary-inverse);
-    border-radius: 0.2rem;
-	}
-
-	.field a:hover {
-		text-decoration: none;
-	}
-
-	.field a:focus-visible {
-		color: var(--color-accent);
-		text-decoration: none;
-	}
-
-	.field p {
-		text-align: center;
-	}
-
-	button[type='submit'] {
-		width: 100%;
-		margin-top: 0.9rem;
-		padding: 0.5rem 1rem;
-		font-weight: bold;
-		color: var(--color-black);
-		background-color: var(--color-success-bg);
 	}
 </style>
