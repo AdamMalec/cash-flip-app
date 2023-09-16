8a09de3860e89753ecf26a23b6489ff9b475809f
Adam Malec <add.malec@gmail.com>
2023-09-16 23:20:19 +0300

    feat: add reset-password ROUTE

---
+++ src/routes/(auth)/reset-password/+page.svelte
@@ -0,0 +1,21 @@
+<div class="auth">
+  <h1 class="auth__title">Reset Password</h1>
+  <form class="auth__form">
+
+    <div class="field">
+      <label for="newPassword">New Password</label>
+      <input type="password" name="newPassword" id="newPassword" required/>
+    </div>
+
+    <div class="field">
+      <label for="confirmPassword">Confirm Password</label>
+      <input type="password" name="confirmPassword" id="confirmPassword" required/>
+    </div>
+
+    <div class="field">
+      <button class="auth__btn" type="submit">Update Password</button>
+      <p><a href="/login">I'm ready to login</a></p>
+    </div>
+
+  </form>
+</div>
