2927258fb621431cd5467571b36fc2c035654fea
Adam Malec <add.malec@gmail.com>
2023-09-16 22:57:51 +0300

    feat: add signup ROUTE

---
+++ src/routes/(auth)/signup/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	let email: string;
+	let password: string;
+</script>
+
+<h1>Sign Up</h1>
+<form class="auth">
+	<div class="field">
+		<label for="fullName">Full Name</label>
+		<input type="text" name="fullName" id="fullName" required/>
+	</div>
+
+	<div class="field">
+		<label for="email">Email</label>
+		<input type="email" name="email" id="email" required/>
+	</div>
+
+  <div class="field">
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password" required/>
+	</div>
+
+  <div class="field">
+		<label for="confirmPassword">Confirm Password</label>
+		<input type="password" name="confirmPassword" id="confirmPassword" required/>
+	</div>
+
+	<div class="field">
+		<button class="auth__btn" type="submit">Sign Up </button>
+		<p>
+			<a href="/signup">Already have an account?</a>
+		</p>
+	</div>
+</form>
+
+<style>
+	h1 {
+		margin-bottom: 2rem;
+		color: var(--color-accent);
+	}
+
+	.field__wrapper {
+		display: flex;
+		justify-content: space-between;
+	}
+</style>
