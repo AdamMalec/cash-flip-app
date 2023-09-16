b366ab83ba5e5598528114a9ea3317ab83af6c30
Adam Malec <add.malec@gmail.com>
2023-09-16 23:19:37 +0300

    refactor: add auth pages styles to global.css #2

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -3,33 +3,30 @@
 	let password: string;
 </script>
 
-<h1>Login</h1>
-<form class="auth">
-	<div class="field">
-		<label for="email">Email</label>
-		<input type="text" name="email" id="email" required bind:value={email} />
-	</div>
-
-	<div class="field">
-		<div class="field__wrapper">
-			<label for="password">Password</label>
-			<a href="/forgot-password">Forgot?</a>
+<div class="auth">
+	<h1 class="auth__title">Login</h1>
+	<form class="auth__form">
+		<div class="field">
+			<label for="email">Email</label>
+			<input type="text" name="email" id="email" required bind:value={email} />
 		</div>
-		<input type="password" name="password" id="password" required bind:value={password} />
-	</div>
 
-	<div class="field">
-		<button class="auth__btn" type="submit">Login</button>
-		<p><a href="/signup">Don't have an account yet?</a></p>
-	</div>
-</form>
+		<div class="field">
+			<div class="field__wrapper">
+				<label for="password">Password</label>
+				<a href="/forgot-password">Forgot?</a>
+			</div>
+			<input type="password" name="password" id="password" required bind:value={password} />
+		</div>
+
+		<div class="field">
+			<button class="auth__btn" type="submit">Login</button>
+			<p><a href="/signup">Don't have an account yet?</a></p>
+		</div>
+	</form>
+</div>
 
 <style>
-	h1 {
-		margin-bottom: 2rem;
-		color: var(--color-accent);
-	}
-
 	.field__wrapper {
 		display: flex;
 		justify-content: space-between;
--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -1,46 +1,29 @@
-<script lang="ts">
-	let email: string;
-	let password: string;
-</script>
+<div class="auth">
+  <h1 class="auth__title">Sign Up</h1>
+  <form class="auth__form">
+    <div class="field">
+      <label for="fullName">Full Name</label>
+      <input type="text" name="fullName" id="fullName" required/>
+    </div>
 
-<h1>Sign Up</h1>
-<form class="auth">
-	<div class="field">
-		<label for="fullName">Full Name</label>
-		<input type="text" name="fullName" id="fullName" required/>
-	</div>
+    <div class="field">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" required/>
+    </div>
 
-	<div class="field">
-		<label for="email">Email</label>
-		<input type="email" name="email" id="email" required/>
-	</div>
+    <div class="field">
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password" required/>
+    </div>
 
-  <div class="field">
-		<label for="password">Password</label>
-		<input type="password" name="password" id="password" required/>
-	</div>
+    <div class="field">
+      <label for="confirmPassword">Confirm Password</label>
+      <input type="password" name="confirmPassword" id="confirmPassword" required/>
+    </div>
 
-  <div class="field">
-		<label for="confirmPassword">Confirm Password</label>
-		<input type="password" name="confirmPassword" id="confirmPassword" required/>
-	</div>
-
-	<div class="field">
-		<button class="auth__btn" type="submit">Sign Up </button>
-		<p>
-			<a href="/signup">Already have an account?</a>
-		</p>
-	</div>
-</form>
-
-<style>
-	h1 {
-		margin-bottom: 2rem;
-		color: var(--color-accent);
-	}
-
-	.field__wrapper {
-		display: flex;
-		justify-content: space-between;
-	}
-</style>
+    <div class="field">
+      <button class="auth__btn" type="submit">Sign Up</button>
+      <p><a href="/login">Already have an account?</a></p>
+    </div>
+  </form>
+</div>
