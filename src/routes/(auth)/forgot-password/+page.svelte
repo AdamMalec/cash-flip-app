f6e03a0d3584b004b6b3571b860e463c5599fc12
Adam Malec <add.malec@gmail.com>
2023-09-16 23:20:07 +0300

    feat: add forgot-password ROUTE

---
+++ src/routes/(auth)/forgot-password/+page.svelte
@@ -0,0 +1,15 @@
+<div class="auth">
+  <h1 class="auth__title">Forgot Password</h1>
+  <form class="auth__form">
+
+    <div class="field">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" required/>
+    </div>
+
+    <div class="field">
+      <button class="auth__btn auth__btn--reset" type="submit">Send a Reset Email</button>
+      <p><a href="/login">Ready to login?</a></p>
+    </div>
+  </form>
+</div>
